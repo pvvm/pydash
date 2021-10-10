bddbc547f959973b20d14bd2f9905df9e1aa154f
Pedro Vitor <pv.mizuno@hotmail.com>
2021-10-10 10:20:21 -0300

    Verificar se aqueles valore sao constantes

--- r2a/r2a_rst.py
+++ r2a/r2a_rst.py
@@ -21,7 +21,7 @@ class R2A_RST(IR2A):
 
         parsed_mpd = parse_mpd(msg.get_payload())   # recebe o payload e faz o parsing
         self.qi = parsed_mpd.get_qi()               # recebe a lista de qualidades
-        
+        print(self.qi)
         #t = time.perf_counter() - self.request_time
         #print(msg.get_payload())
         #print("\n\nTEMPO DE RESPOSTA", t, "\n\n")
@@ -42,25 +42,74 @@ class R2A_RST(IR2A):
         #msg.add_quality_id(selected_qi)
 
         #print("FETCH:      ", self.last_fetch)
-        mi = 1/self.last_fetch                                      # mi = msd/sft (considerando que a duração do segmento é sempre 1)
-        print("MI: ", mi)
-        qi_tuple = self.whiteboard.get_playback_qi()
+        mi = 1/self.last_fetch                      # mi = msd/sft (considerando que a duração do segmento é sempre 1)
+        #print("MI: ", mi)
         epsilon = 0
-        # calcula o epsilon
-        if len(qi_tuple) > 0:
+
+        qi_tuple = self.whiteboard.get_playback_qi()
+        if len(qi_tuple) == 0:
+            quality_level = 1
+        else:
             quality_level = qi_tuple[-1][1]
-            if quality_level < 19:
+            if quality_level < 19:                  # calcula o epsilon
                 epsilon = (self.qi[quality_level + 1] - self.qi[quality_level]) / self.qi[quality_level]
             else:
                 epsilon = 99999
-        
         #print("EPSILON: ", epsilon)
 
+        # Verificar se podem ser constantes
+        buf_min = 3
+        buf_reduce = 8
+        buf_safety = 12
+        gamma = 0.8
+
         buffer_tuple = self.whiteboard.get_playback_buffer_size()   # tupla do tempo do buffer analisado e seu tamanho
-        #if len(buffer_tuple) > 0:
-        #    print("AQUI: ",buffer_tuple[-1][1])   # tamanho mais recente do buffer
+        
+        if len(buffer_tuple) > 0:
+            print("A")
+            buf_c = buffer_tuple[-1][1]     # tamanho atual do buffer
+
+            if buf_c < buf_min:
+                print("B")
+                if mi >= 1:
+                    print("C")
+                    if quality_level == 0:
+                        msg.add_quality_id(self.qi[quality_level])
+                    else:
+                        msg.add_quality_id(self.qi[quality_level - 1])
+                else:
+                    print("D")
+                    index = -1
+                    for i in self.qi:
+                        if i < self.qi[quality_level] * mi:
+                            index += 1
+                    if index >= 0:
+                        msg.add_quality_id(self.qi[index])
+                    else:
+                        msg.add_quality_id(self.qi[0])
             
-        msg.add_quality_id(self.qi[19])            # instrução que define a qualidade
+            elif mi < gamma and buf_c < buf_reduce:
+                print("E")
+                if quality_level == 0:
+                    msg.add_quality_id(self.qi[quality_level])
+                else:
+                    msg.add_quality_id(self.qi[quality_level - 1])
+            
+            elif mi > (1 + epsilon) and buf_c > buf_safety:
+                print("F")
+                if quality_level == 19:
+                    msg.add_quality_id(self.qi[quality_level])
+                else:
+                    msg.add_quality_id(self.qi[quality_level + 1])
+            
+            else:
+                print("G")
+                msg.add_quality_id(self.qi[quality_level])
+
+        else:
+            print("H")
+            msg.add_quality_id(self.qi[0])
+
         self.send_down(msg)
 
     def handle_segment_size_response(self, msg):
